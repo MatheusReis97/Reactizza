c19c273e8df00508bb8866e133be86bb1c2050f5
Matheus Reis <matheus.drs97@hotmail.com>
2022-11-22 01:04:22 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function iniciarCadastro(){
+
+  document.getElementById("login").style.display="none";
+  document.getElementById("show").style.display="inline-block";
+}
+
+function continuarCadastro(){
+  document.getElementById("hidden").style.display="inline-block";
+  document.getElementById("show").style.display="none";
+}
